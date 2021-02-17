4d0a5066be1417561579bfdb50c4b68a5696c6c5
Jabbar <multimediary@gmail.com>
2021-02-17 17:26:35 +0700

    Revisi Cache

--- application/views/v_header.php
+++ application/views/v_header.php
@@ -4,9 +4,6 @@
 <head>
     <title>Ma'shum Abdul Jabbar | <?php echo $this->router->fetch_class();?></title>
     <meta charset="UTF-8">
-	<meta http-equiv="Cache-Control" content="no-cache, no-store, must-revalidate" />
-	<meta http-equiv="Pragma" content="no-cache" />
-	<meta http-equiv="Expires" content="0" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="keywords"
         content="portofolio, portfolio, Ma'shum, Abdul, Jabbar, Ma'shum Abdul Jabbar, ECS, AlibabaCloud, Codepolitan, re-cloud" />
