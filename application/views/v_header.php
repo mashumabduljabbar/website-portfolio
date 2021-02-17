c1666afc585057b172df7e9c65c0f7056b00b9df
Jabbar <multimediary@gmail.com>
2021-02-17 18:07:13 +0700

    Optimize 2

--- application/views/v_header.php
+++ application/views/v_header.php
@@ -41,7 +41,7 @@
     <!-- Favicon -->
     <!-- Font awesome icons -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css" integrity="sha512-+4zCK9k+qNFUR5X+cKL9EIR+ZOhtIloNl9GIKS57V1MyNsYpYcUrUeQc9vNfzsWfV28IaLL3i96P9sdNyeRssA==" crossorigin="anonymous" />
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/jpswalsh/academicons@1/css/academicons.min.css">
+    <!-- <link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/jpswalsh/academicons@1/css/academicons.min.css">-->
 	<!-- Google Analytics -->
     <script async src="https://www.googletagmanager.com/gtag/js?id=G-CJCSZP1ZVN"></script>
 	<script>
