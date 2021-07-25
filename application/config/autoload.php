abff7924b551df18238d7379b8063eb1ff3df75d
Jabbar <multimediary@gmail.com>
2021-07-25 09:10:10 +0700

    Remove DB

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -58,7 +58,7 @@ $autoload['packages'] = array();
 |
 |	$autoload['libraries'] = array('user_agent' => 'ua');
 */
-$autoload['libraries'] = array('database', 'session', 'upload', 'email', 'form_validation');
+$autoload['libraries'] = array('session', 'upload', 'email', 'form_validation');
 
 /*
 | -------------------------------------------------------------------
