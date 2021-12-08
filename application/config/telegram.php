1caafec13d0d814396db79456f021023fa5dc182
Ma'shum Abdul Jabbar <multimediary@gmail.com>
2021-12-08 23:57:03 +0700

    Add Telegram File

---
+++ application/config/telegram.php
@@ -0,0 +1,5 @@
+<?php
+    $config['telegram_bot_id'] = 'CHANGE TO YOUR BOT ID';
+    $config['telegram_bot_key'] = 'CHANGE TO YOUR BOT KEY';
+    $config['telegram_bot_name'] = 'CHANGE TO YOUR BOT NAME';
+    $config['multimediary'] = 'CHANGE TO YOUR ID TO SEND MESSAGE';
